987908d966d49fcc4cb355192bd0f2e1f4cc5feb
VasilisStromatias <stromatiasvasilis@gmail.com>
2021-04-05 14:40:23 +0300

    Burger Menu done for mobile devices

--- src/pages/NavBar.js
+++ src/pages/NavBar.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Link} from "react-router-dom";
 import '../css/NavBar.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 function  NavBar() {
+
+    const [toggle, setToggle]= useState (false);
+
+
 return (
 <>
 
 <nav className ="nav nav-pills navbar-expand-md bg-dark 
- align-items-center justify-content-between sticky-top">   
+ align-items-center justify-content-between">   
 
- <Link className="navbar-brand text-warning ml-5 company-name d-none d-sm-block" to="/">Upupa epops</Link>
+        <button className="btn btn-warning d-sm-none menu-toggle-btn m-2"
+        onClick={()=> {
+            setToggle(!toggle);
+            }}>
+            ---
+        </button>
 
-<div className="mx-auto mx-sm-0">
-    <ul className = "list-items navbar-nav bg-dark text-center" >    
-        <li><Link className="nav-link text-warning mr-4" to="/">Home</Link></li>
-        <li><Link className="nav-link text-warning mr-4" to="/signature-cocktail">Signature Cocktail</Link></li>
-        <li><Link className="nav-link text-warning mr-4" to="/food-menu">Food Menu</Link></li>
-        <li><Link className="nav-link text-warning mr-4" to="/contact">Contact Us</Link></li>
-        <li><Link className="nav-link text-warning mr-4" to="/test">TestPage</Link></li>
-    </ul> 
-</div>   
+
+<Link className="navbar-brand text-warning ml-5 company-name d-none d-sm-block" to="/">Upupa epops</Link>
+
+    <div className={
+        'mx-auto mx-sm-0 d-sm-block ' + 
+        (toggle === false ? 'd-none ' : ' ')
+    }>
+        <ul className = "list-items navbar-nav bg-dark text-center" >    
+            <li><Link className="nav-link text-warning mr-4" to="/">Home</Link></li>
+            <li><Link className="nav-link text-warning mr-4" to="/signature-cocktail">Signature Cocktail</Link></li>
+            <li><Link className="nav-link text-warning mr-4" to="/food-menu">Food Menu</Link></li>
+            <li><Link className="nav-link text-warning mr-4" to="/contact">Contact Us</Link></li>
+            <li><Link className="nav-link text-warning mr-4" to="/test">TestPage</Link></li>
+        </ul> 
+    </div>  
+
 
 </nav>
 </>
